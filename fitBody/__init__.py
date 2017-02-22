b287322ff2da3a6e895b047b68ea9fe083186c43
kaistullich <kairstullich@gmail.com>
2017-02-22 08:11:41 -0800

    implemented WYSIWYG editor, added descriptipon in DB

--- fitBody/__init__.py
+++ fitBody/__init__.py
@@ -1,9 +1,13 @@
 import os
+import fitBody.views
 from flask import Flask
 from fitBody.views import my_view
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.contrib import sqla
+from wtforms import fields, widgets
+
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
@@ -28,21 +32,36 @@ db = SQLAlchemy(app)
 admin = Admin(app, template_mode='bootstrap3')
 
 
+# Create the WYSIWYG editor
+class CKTextAreaWidget(widgets.TextArea):
+    def __call__(self, field, **kwargs):
+        # add WYSIWYG class to existing classes
+        existing_classes = kwargs.pop('class', '') or kwargs.pop('class_', '')
+        kwargs['class'] = u'%s %s' % (existing_classes, "ckeditor")
+        return super(CKTextAreaWidget, self).__call__(field, **kwargs)
+
+
+class CKTextAreaField(fields.TextAreaField):
+    widget = CKTextAreaWidget()
+
+
 # Create models (User Registration DB)
 class Registration(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(50))
     username = db.Column(db.String(20))
     hash = db.Column(db.String(350))
+    description = db.Column(db.UnicodeText())
 
 
 # Create class to be able to use the WYSIWYG editor
 class RegistrationEdit(ModelView):
+    form_overrides = dict(description=CKTextAreaField)
     create_template = 'create.html'
     edit_template = 'edit.html'
 
     # Formats the description columns since it will be very long
-    def _description_formatter(view, model):
+    def _description_formatter(view, context, model, name):
         # If the description column is empty it will place an empty string for formatting purposes
         if model.description is None:
             return ""
